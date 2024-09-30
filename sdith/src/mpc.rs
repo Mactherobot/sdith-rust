5cacbb818255bfac9da03711090a31f59d271260
Magnus Jensen <mnje@trifork.com>
2024-09-30 11:40:00 +0200

    [mpc]: Add the correct creation of a,b,c bobr triple

--- sdith/src/mpc.rs
+++ sdith/src/mpc.rs
@@ -3,36 +3,53 @@ use crate::{
         gf256::gf256_ext::{gf256_ext32_mul, gf256_ext32_sample},
         vectors::{self, serialize},
     },
-    constants::params::{PARAM_EXT_DEGREE, PARAM_NB_EVALS_PER_POLY, PARAM_SPLITTING_FACTOR},
+    constants::params::{
+        PARAM_EXT_DEGREE, PARAM_NB_EVALS_PER_POLY, PARAM_SEED_SIZE, PARAM_SPLITTING_FACTOR,
+    },
     subroutines::prg::prg::PRG,
 };
 
 #[derive(Debug)]
-pub(crate) struct Mpc {
+pub(crate) struct MPC {
     pub(crate) a: [[[u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR],
     pub(crate) b: [[[u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR],
     pub(crate) c: [[u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY],
 }
 
-impl Mpc {
+impl MPC {
     pub(crate) fn new() -> Self {
-        let a = [[[0; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR];
-        let b = [[[0; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR];
-        let c = [[0; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY];
-
-        Mpc { a, b, c }
-    }
-
-    /// Computes the beaver triples to be sacrificed in the mpc protocol
-    pub(crate) fn compute_correlated(&self) -> [[u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY] {
-        // Iterate over all the a b values
-        let mut c = self.c;
+        let a = [[gf256_ext32_sample(&mut PRG::init(&[0u8; PARAM_SEED_SIZE], None));
+            PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR];
+        let b = [[gf256_ext32_sample(&mut PRG::init(&[0u8; PARAM_SEED_SIZE], None));
+            PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR];
+        let mut c = [[0u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY];
         (0..PARAM_NB_EVALS_PER_POLY).for_each(|i| {
             // Update the c value
             for j in 0..PARAM_SPLITTING_FACTOR {
-                c[i] = gf256_ext32_mul(self.a[j][i], self.b[j][i]);
+                c[i] = gf256_ext32_mul(a[j][i], b[j][i]);
             }
         });
-        c
+
+        MPC { a, b, c }
+    }
+}
+
+#[cfg(test)]
+mod mpc_tests {
+    use super::*;
+
+    #[test]
+    fn test_mpc() {
+        let mpc = MPC::new();
+        assert_ne!(
+            mpc.a,
+            [[[0u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR]
+        );
+        assert_ne!(
+            mpc.b,
+            [[[0u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY]; PARAM_SPLITTING_FACTOR]
+        );
+        assert_ne!(mpc.c, [[0u8; PARAM_EXT_DEGREE]; PARAM_NB_EVALS_PER_POLY]);
+        assert_eq!(mpc.c[0], gf256_ext32_mul(mpc.a[0][0], mpc.b[0][0]));
     }
 }
