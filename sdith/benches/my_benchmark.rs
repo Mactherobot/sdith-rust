53272329751b4c61575c57f41f4d162b2b5d0e7c
Benjamin <benjamin.zachariae@gmail.com>
2024-11-11 13:30:26 +0100

    Fix bench

--- sdith/benches/my_benchmark.rs
+++ sdith/benches/my_benchmark.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use criterion_cycles_per_byte::CyclesPerByte;
 use nist_pqc_seeded_rng::{NistPqcAes256CtrRng, RngCore, Seed, SeedableRng};
+use sdith::constants::params::{PARAM_DIGEST_SIZE, PARAM_SEED_SIZE};
 use sdith::keygen::keygen;
 use sdith::keygen::{PublicKey, SecretKey};
 use sdith::signature::signature::Signature;
@@ -12,9 +12,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     rng.fill_bytes(&mut keygen_seed);
     c.bench_function("keygen", |b| b.iter(|| keygen_bench(&mut rng)));
 
-    let (pk, sk): (PublicKey, SecretKey) = keygen(keygen_seed);
+    let (pk, sk) = keygen(keygen_seed);
 
-    let mut sign_seed = [0u8; 16];
+    let mut sign_seed = [0u8; PARAM_SEED_SIZE];
     let mut sign_salt = [0u8; PARAM_DIGEST_SIZE];
     rng.fill_bytes(&mut sign_seed);
     rng.fill_bytes(&mut sign_salt);
@@ -22,12 +22,12 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     let message: Vec<u8> = vec![1, 2, 3, 4];
     c.bench_function("signing", |b| {
-        b.iter(|| signing_bench(entropy, sk, message.clone()))
+        b.iter(|| signing_bench(entropy, *sk, message.clone()))
     });
 
-    let signature: Vec<u8> = Signature::sign_message(entropy, sk, &message).unwrap();
+    let signature: Vec<u8> = Signature::sign_message(entropy, &sk, &message).unwrap();
     c.bench_function("verification", |b| {
-        b.iter(|| verification_bench(pk, &signature))
+        b.iter(|| verification_bench(*pk, &signature))
     });
 }
 
@@ -38,16 +38,17 @@ fn keygen_bench(rng: &mut NistPqcAes256CtrRng) {
 }
 
 fn signing_bench(entropy: ([u8; 16], [u8; PARAM_DIGEST_SIZE]), sk: SecretKey, message: Vec<u8>) {
-    let _signature = Signature::sign_message(entropy, sk, &message.to_vec());
+    let _signature = Signature::sign_message(entropy, &sk, &message.to_vec());
 }
 
 fn verification_bench(pk: PublicKey, signature: &Vec<u8>) {
-    let _verification = Signature::verify_signature(pk, signature);
+    let _verification = Signature::verify_signature(&pk, signature);
 }
 
-criterion_group!(
-    name = my_bench;
-    config = Criterion::default().with_measurement(CyclesPerByte);
-    targets = bench
-);
+// criterion_group!(
+//     name = my_bench;
+//     config = Criterion::default().with_measurement(CyclesPerByte);
+//     targets = bench
+// );
+criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
